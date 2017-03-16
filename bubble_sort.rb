687b659efeab8b40e14504ba90b0ea100aeefb8d
Christopher Steingraber <steingraberchristopher@yahoo.com>
2017-03-16 08:03:36 -0400

    Create and test bubble_sort_by method

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -14,5 +14,26 @@ def bubble_sort(array)
 	array
 end
 
+def bubble_sort_by(array)
+	(array.length - 1).times do |iteration|
+		sorted = true
+		(array.length - (1 + iteration)).times do |index|
+			if yield(array[index], array[index + 1]) > 0
+				array[index], array[index + 1] = array[index + 1], array[index]
+				sorted = false
+			end
+		end
+		if sorted
+			return array
+		end
+	end
+	array
+end
+
 answer = bubble_sort([3, 2, 1, 5, 3, 1, 2])
 p answer
+
+answer2 = bubble_sort_by(["Christopher", "Breana", "Sinai"]) do |left, right|
+	left.length - right.length
+end
+p answer2
